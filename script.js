e09a03477c5dfd279dc80c5bf41b65514daa5180
Aswin-P-Kurian <aswinpkurian@gmail.com>
2025-10-07 11:39:09 +0530

    Initial commit: Add calculator files

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let display = document.getElementById('result');
+
+function clearScreen() {
+  display.value = '';
+}
+
+function backspace() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
